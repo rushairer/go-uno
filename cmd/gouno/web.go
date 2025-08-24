894468965a3513fc37b5b6e537ab9ce0de991bf6
Aben <me@aben.io>
2025-08-25 00:47:38 +0800

    feat: 完善gin server的初始化

--- cmd/gouno/web.go
+++ cmd/gouno/web.go
@@ -10,7 +10,8 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/rushairer/go-uno/internal/core"
+	"github.com/rushairer/go-uno/gouno"
+	"github.com/rushairer/go-uno/internal/router"
 	"github.com/spf13/cobra"
 )
 
@@ -31,16 +32,16 @@ func init() {
 func startWebServer(cmd *cobra.Command, args []string) {
 	log.Printf("starting web server...")
 
-	err := core.InitConfig(cmd.Flag("config").Value.String())
+	err := gouno.InitConfig(cmd.Flag("config").Value.String())
 	if err != nil {
 		log.Fatalf("init config failed, err: %v", err)
 	}
 
-	core.GlobalConfig.WebServerConfig.Address = cmd.Flag("address").Value.String()
-	core.GlobalConfig.WebServerConfig.Port = cmd.Flag("port").Value.String()
-	core.GlobalConfig.WebServerConfig.Debug, _ = cmd.Flags().GetBool("debug")
+	gouno.GlobalConfig.WebServerConfig.Address = cmd.Flag("address").Value.String()
+	gouno.GlobalConfig.WebServerConfig.Port = cmd.Flag("port").Value.String()
+	gouno.GlobalConfig.WebServerConfig.Debug, _ = cmd.Flags().GetBool("debug")
 
-	if core.GlobalConfig.WebServerConfig.Debug {
+	if gouno.GlobalConfig.WebServerConfig.Debug {
 		gin.SetMode(gin.DebugMode)
 	} else {
 		gin.SetMode(gin.ReleaseMode)
@@ -50,21 +51,20 @@ func startWebServer(cmd *cobra.Command, args []string) {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	server := gin.Default()
-
-	testGroup := server.Group("/test")
-	{
-		testGroup.GET(
-			"/alive",
-			func(ctx *gin.Context) {
-				ctx.String(http.StatusOK, "pong")
-			},
-		)
-	}
+	engine := gin.Default()
+	engine.Use(
+		gouno.RecoveryMiddleware(),
+		gouno.TimeoutMiddleware(),
+	)
+	router.RegisterWebRouter(engine)
 
 	httpServer := &http.Server{
-		Addr:    fmt.Sprintf("%s:%s", core.GlobalConfig.WebServerConfig.Address, core.GlobalConfig.WebServerConfig.Port),
-		Handler: server,
+		Addr:              fmt.Sprintf("%s:%s", gouno.GlobalConfig.WebServerConfig.Address, gouno.GlobalConfig.WebServerConfig.Port),
+		IdleTimeout:       gouno.GlobalConfig.WebServerConfig.IdleTimeout,
+		WriteTimeout:      gouno.GlobalConfig.WebServerConfig.WriteTimeout,
+		ReadTimeout:       gouno.GlobalConfig.WebServerConfig.ReadTimeout,
+		ReadHeaderTimeout: gouno.GlobalConfig.WebServerConfig.ReadHeaderTimeout,
+		Handler:           engine,
 	}
 
 	log.Printf("listening on %s", httpServer.Addr)
